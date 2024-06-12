4e74bd77202170f7d82f073de20ba6a53c8dae67
anet-eny <anet.eny@gmail.com>
2024-06-12 14:30:32 +0200

    Create handleRetweetClick() and add click logic

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ document.addEventListener('click', function(e){
     if(e.target.dataset.like){
         handleLikeClick(e.target.dataset.like)
     }
+    else if(e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
       
 })
 
@@ -22,10 +25,27 @@ function handleLikeClick(tweetId){
     
     if(targetTweetObj.isLiked){
         targetTweetObj.likes--
-    } else {
+    } 
+    else {
         targetTweetObj.likes++
     }
     targetTweetObj.isLiked = !targetTweetObj.isLiked
+
+    render()
+}
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweetId === tweet.uuid
+    })[0]
+
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else {
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
 
     render()
 }
